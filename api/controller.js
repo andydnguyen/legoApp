f6f6ecb5ebd28afbc5e96582263b876b282c84fb
Andy Nguyen <anguyen1119@gmail.com>
2019-11-15 19:21:32 -0500

    Add controller logic

--- api/controller.js
+++ api/controller.js
@@ -1,8 +1,22 @@
 'use strict';
 
-const controller = require('./controller');
+var properties = require('../package.json');
+var distance = require('../service/distance');
 
-module.exports = function(app) {
-  app.route('/about').get(controller.about);
-  app.route('/distance/:zipcode1/:zipcode2').get(controller.getDistance);
+var controllers = {
+  about: function(req, res) {
+    var aboutInfo = {
+      name: properties.name,
+      version: properties.version
+    };
+    res.json(aboutInfo);
+  },
+  getDistance: function(req, res) {
+    distance.find(req, res, function(err, dist) {
+      if (err) res.send(err);
+      res.json(dist);
+    });
+  }
 };
+
+module.exports = controllers;
