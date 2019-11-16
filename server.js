e801cfe555b8017448af059d9fae3cd805c7c546
Andy Nguyen <anguyen1119@gmail.com>
2019-11-15 20:53:34 -0500

    Fix server

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
-const route = require('./api/routes');
+const routes = require('./api/routes');
 routes(app);
 
 app.listen(port, () => console.log(`Server started on port: ${port}!`));
